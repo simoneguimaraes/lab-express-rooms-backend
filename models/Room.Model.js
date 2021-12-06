963aef59c8aaf216fbaa3f0465a92740b2038634
Simone Guimaraes <simoneguimaraes11@gmail.com>
2021-12-06 09:13:04 -0300

    inserted routes

--- models/Room.Model.js
+++ models/Room.Model.js
@@ -4,7 +4,14 @@ const RoomSchema = new mongoose.Schema({
   name: { type: String },
   description: { type: String },
   imageUrl: { type: String },
-  reviews: [], 
+  reviews: [],
+});
+
+
+
+const reviewSchema = new Schema({
+  comment: { type: String, maxlength: 200 },
+  roomId: { type: Schema.Types.ObjectId, ref: "Room" },
 });
 
 module.exports = mongoose.model("Room", RoomSchema);
--- routes/room.routes.js
+++ routes/room.routes.js
@@ -4,13 +4,46 @@ const router = express.Router();
 
 const RoomModel = require("../models/Room.Model");
 
+//create new rooms
+//edit and delete the rooms
+//see the list of the rooms
 
+//POST - Criar produto ---> C
 
+router.post("/room", (req, res) => {
+  RoomModel.create(req.body)
+    .then((result) => res.status(201).json(result))
+    .catch((err) => console.log(err));
+});
 
+//GET - Ler o banco de dados ---> R
 
+router.get("/room", (req, res) => {
+  RoomModel.find()
+    .then((result) => res.status(200).json(result))
+    .catch((err) => console.log(err));
+});
 
+//UPDATE - Trocar produto ---> U
+//put - troca o objeto inteiro
+//patch - troca um item em específico
 
+router.patch("/room/:id", (req, res) => {
+  RoomModel.findOneAndUpdate(
+    { _id: req.params.id },
+    { $set: req.body },
+    { new: true, runValidators: true }
+  )
+    .then((result) => res.status(200).json(result))
+    .catch((err) => console.log(err));
+});
 
+//DELETE - Deletar produto ---> D
 
+router.delete("/room/:id", (req, res) => {
+  RoomModel.deleteOne({ _id: req.params.id })
+    .then((result) => res.status(200).json({}))
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
